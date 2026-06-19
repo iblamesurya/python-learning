073ca2abce9f793e73c3b7cbaa0f155112e0c973
surya tummala <nothinsurya@gmail.com>
2026-06-19 17:06:41 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,1 +1,7 @@
-#sorry for today
+#currency converter
+# Write code below 💖
+pesos = float(input("What do you have left in pesos?"))
+soles = float(input("What do you have left in soles?"))
+reais = float(input("What do you have left in reais?"))
+usd = (pesos*0.00029 + soles*0.30 + reais*0.19) 
+print(usd)
